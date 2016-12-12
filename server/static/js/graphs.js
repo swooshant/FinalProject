993d27b90870052e4e14ccd225daff4a065d3abf
Jacob Melton <jmelton@redhat.com>
2016-12-12 02:01:56 -0500

    Add more library files, update the template with new field and graph names
    Update it so charts are now correctly labeled in graph.js

--- server/static/js/graphs.js
+++ server/static/js/graphs.js
@@ -33,86 +33,76 @@ function makeGraphs(error, apsJson) {
 	var nameGroup = nameDim.group();
 	var all = ndx.groupAll();
 
-	//Calculate metrics
-	var numProjectsByDate = dateDim.group(); 
-	var numProjectsByResourceType = resourceTypeDim.group();
-	var numProjectsByPovertyLevel = povertyLevelDim.group();
-	var totalDonationsByState = stateDim.group().reduceSum(function(d) {
-		return d["total_donations"];
-	});
-
-	var all = ndx.groupAll();
-	var totalDonations = ndx.groupAll().reduceSum(function(d) {return d["total_donations"];});
-
-	var max_state = totalDonationsByState.top(1)[0].value;
-
 	//Define values (to be used in charts)
-	var minDate = dateDim.bottom(1)[0]["date_posted"];
-	var maxDate = dateDim.top(1)[0]["date_posted"];
+	var minDate = timeDim.bottom(1)[0]["timestamp"];
+	var maxDate = timeDim.top(1)[0]["timestamp"];
 
-    //Charts
+	// Charts
+	var numberRecordsND = dc.numberDisplay("#number-records-nd");
 	var timeChart = dc.barChart("#time-chart");
-	var resourceTypeChart = dc.rowChart("#resource-type-row-chart");
-	var povertyLevelChart = dc.rowChart("#poverty-level-row-chart");
-	var usChart = dc.geoChoroplethChart("#us-chart");
-	var numberProjectsND = dc.numberDisplay("#number-projects-nd");
-	var totalDonationsND = dc.numberDisplay("#total-donations-nd");
+	var encryptionChart = dc.rowChart("#encryption-row-chart");
+	var channelChart = dc.rowChart("#channel-row-chart");
+	var addressChart = dc.rowChart("#address-row-chart");
+	var nameChart = dc.rowChart("#name-row-chart");
 
+	// Pass necessary parameters
 	numberProjectsND
 		.formatNumber(d3.format("d"))
 		.valueAccessor(function(d){return d; })
 		.group(all);
 
-	totalDonationsND
-		.formatNumber(d3.format("d"))
-		.valueAccessor(function(d){return d; })
-		.group(totalDonations)
-		.formatNumber(d3.format(".3s"));
-
 	timeChart
-		.width(600)
-		.height(160)
-		.margins({top: 10, right: 50, bottom: 30, left: 50})
-		.dimension(dateDim)
-		.group(numProjectsByDate)
+		.width(650)
+		.height(140)
+		.margins({top: 10, right: 50, bottom: 20, left: 20})
+		.dimension(timeDim)
+		.group(numRecordsByTime)
 		.transitionDuration(500)
 		.x(d3.time.scale().domain([minDate, maxDate]))
 		.elasticY(true)
-		.xAxisLabel("Year")
 		.yAxis().ticks(4);
 
-	resourceTypeChart
-        .width(300)
-        .height(250)
-        .dimension(resourceTypeDim)
-        .group(numProjectsByResourceType)
-        .xAxis().ticks(4);
-
-	povertyLevelChart
+	encryptionChart
 		.width(300)
-		.height(250)
-        .dimension(povertyLevelDim)
-        .group(numProjectsByPovertyLevel)
-        .xAxis().ticks(4);
+		.height(100)
+	        .dimension(encryptionDim)
+	        .group(encryptionGroup)
+	        .ordering(function(d) { return -d.value })
+	        .colors(['#6baed6'])
+	        .elasticX(true)
+	        .xAxis().ticks(4);
 
+	 channelChart
+		.width(300)
+		.height(150)
+		.dimension(channelDim)
+        	.group(channelGroup)
+        	.colors(['#6baed6'])
+        	.elasticX(true)
+        	.labelOffsetY(10)
+        	.xAxis().ticks(4);
 
-	usChart.width(1000)
-		.height(330)
-		.dimension(stateDim)
-		.group(totalDonationsByState)
-		.colors(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"])
-		.colorDomain([0, max_state])
-		.overlayGeoJson(statesJson["features"], "state", function (d) {
-			return d.properties.name;
-		})
-		.projection(d3.geo.albersUsa()
-    				.scale(600)
-    				.translate([340, 150]))
-		.title(function (p) {
-			return "State: " + p["key"]
-					+ "\n"
-					+ "Total Donations: " + Math.round(p["value"]) + " $";
-		})
+	addressChart
+		.width(300)
+		.height(310)
+		.dimension(addressDim)
+		.group(addressGroup)
+		.ordering(function(d) { return -d.value })
+		.colors(['#6baed6'])
+		.elasticX(true)
+		.xAxis().ticks(4);
+
+	nameChart
+		.width(200)
+		.height(510)
+		.dimension(nameDim)
+		.group(nameGroup)
+		.ordering(function(d) { return -d.value })
+		.colors(['#6baed6'])
+		.elasticX(true)
+		.labelOffsetY(10)
+		.xAxis().ticks(4);
+
 
     dc.renderAll();
 
