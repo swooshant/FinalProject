9591851ea18ace9f058ee1b6e3b652b231ae4325
Sushant Bhattarai <swooshant@gmail.com>
2016-12-13 19:25:47 -0500

    Original gps code

--- gpsPi.py
+++ gpsPi.py
@@ -6,87 +6,67 @@ import socket
 import sys
 import uuid
 import time
+from datetime import datetime
 
+username = 'team16'
+password = 'ece4564'
+address = '127.0.0.1'
+port = 5672
+queue_name = 'brogrammers'
 
-# class gpsRPi(object):
-	
-	# def __init__(self, address, port, queue_name, username, password):
+latitude = 37.232944
+longitude = -80.422957
 
-	#     self.queue_name = queue_name
-	#     print("Setup Rabbitmq connection")
-	#     #self.credentials = pika.PlainCredentials('team16', 'ece4564')
-	#     #queue_name = 'brogrammers'
+def on_request(ch, method, props, body):
+		global latitude
+		global longitude
+		jsonReply = {}
+		try:
+			packet = gpsd.get_current()
+			# latitude = latitude + .0005
+			# longitude = longitude + .0005
+			# print(packet.position())
+			jsonReply['position'] = packet.position()
+			jsonReply['time'] = packet.time()
+			jsonReply['mapURL'] = packet.map_url()
+			# jsonReply['time'] = str(datetime.now())
+			# jsonReply['longitude'] = longitude
+			# jsonReply['latitude'] = latitude
+			reply = json.dumps(jsonReply)
 
-	#     #set the creditials and connection parameters for the message queue
-	#     self.credentials = pika.PlainCredentials(username, password)
-	#     self.connection = pika.BlockingConnection(pika.ConnectionParameters(address,
-	#                                                                         port,
-	#                                                                         '/',
-	#                                                                         self.credentials))
+		except Exception as e:
+			print("Except: " + str(e))
+			reply = "error"
 
-	#     self.channel = self.connection.channel()
-	#     result = self.channel.queue_declare(exclusive=True)
-	#     self.callback_queue = result.method.queue
+		print(reply)
 
-	#     self.channel.basic_consume(self.on_response, no_ack=True,
-	#                                queue=self.callback_queue)
-	# #queue callback
-	# def on_response(self, ch, method, props, body):
-	#     if self.corr_id == props.correlation_id:
-	#     #the message queue will send an id back that is switched
-	#         self.response = body
+		ch.basic_publish(exchange='', 
+						routing_key=props.reply_to,
+						properties=pika.BasicProperties(correlation_id = \
+						props.correlation_id),
+						body=reply)
+		ch.basic_ack(delivery_tag = method.delivery_tag)
 
-	# #this is called to push and pull to message queue
-	# def call(self, gpsData):
-	#     self.response = None
-	#     self.corr_id = str(uuid.uuid4())
-	#     self.channel.basic_publish(exchange='',
-	#                                routing_key=self.queue_name,
-	#                                properties=pika.BasicProperties(
-	#                                    reply_to=self.callback_queue,
-	#                                    correlation_id=self.corr_id),
-	#                                body=gpsData)
-	#     #break when all the messages are read for pull
-	#     while self.response is None:
-	#         self.connection.process_data_events()
-	#     return self.response
+if __name__ == '__main__':
+	# setup GPS
+	gpsd.connect()
 
-	#  def getGpsInfo():
-	#  	# Connect to the local gpsd
-	# 	gpsd.connect()
-	# 	packet = gpsd.get_current()
-	# 	print(packet.position())
-
-	 	# gpsData['lat'] = gpsd.fix.latitude
-	 	# gpsData['long'] = gpsd.fix.longitude
-	 	# gpsData['utc'] = gpsd.utc,' + ', gpsd.fix.time
-	 	# gpsData['sats'] = gpsd.satellites
-
-
-#message queue setup
-# address = '172.31.61.87'
-# port = 5672
-# username = 'team16'
-# password = 'ece4564'
-# queue_name = 'brogrammers'
-
-# # Setup rpc client
-# gpsData = gpsRPi(address, port, queue_name, username, password)
-
-# try:
-while 1:
-	try :
-		gpsd.connect()
-		packet = gpsd.get_current()
-		print(packet.position())
-		
-	except Exception as e: 
-		print("No fix found")
-		print(e)
-	time.sleep(5)
-	      
-# except KeyboardInterrupt:
-#     print("Closing socket and channel")
-#     s.close()
-#     bridgeRPC.connection.close()
-
+	# Connect to RabbitMQ
+	credentials = pika.PlainCredentials(username=username, password=password)
+	connection =  pika.BlockingConnection(pika.ConnectionParameters(host=address,
+																	port=port,
+																	credentials=credentials))
+	channel = connection.channel()
+	channel.queue_declare(queue=queue_name)
+	# Begin listening to queue
+	channel.basic_qos(prefetch_count=1)
+	channel.basic_consume(on_request, queue=queue_name)
+	print(' [*] Waiting for messages. To exit press CTRL+C')
+	# Start consuming messages
+	channel.start_consuming()
+	try:
+		while True:
+			sleep(2.5)
+	except KeyboardInterrupt:
+		channel.close()
+		connection.close()
